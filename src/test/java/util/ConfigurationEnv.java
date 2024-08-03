d1c65f3ae60bcc6e8d609b768939ff3826a97a2b
eynar-pari <eynar_repc@hotmail.com>
2024-08-03 12:06:00 -0400

    restassured + cucumber

---
+++ src/test/java/util/ConfigurationEnv.java
@@ -0,0 +1,8 @@
+package util;
+
+public class ConfigurationEnv {
+    public static String host ="https://todo.ly";
+    public static String user="catov5@catov5.com";
+    public static String pwd="12345";
+
+}
